696efc6203ee5430138e43107a136e0e44c45cc0
youceftigre <61406016+youceftigre@users.noreply.github.com>
2026-04-23 00:59:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
 import google.generativeai as genai
